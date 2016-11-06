08378060e2649b93ff445533a3754e7f2ce73865
Jibreil <jibreilhoneine@gmail.com>
2016-11-06 18:41:42 +1100

    made comments easier to read

--- Email Alert/vnstat_monitor_email.py
+++ Email Alert/vnstat_monitor_email.py
@@ -4,14 +4,14 @@ import os
 import commands
 import smtplib
 
-#limit in GB and conversion to GiB
+# limit in GB and conversion to GiB
 limitGB = "100"
 
 limitGB = int(limitGB)
 limit = limitGB * 0.93
 limit = str(limit)
 
-#email settings
+# email settings
 sender = 'sender@senderdomain.com'
 receivers = ['to@todomain.com']
 
@@ -22,23 +22,23 @@ Subject: Bandwidth Usage Limit Reached
 Bandwidth usage limit has been reached and the machine will commence shutdown
 """
 
-#updating vnStat database
+# updating vnStat database
 os.system("sudo vnstat -u")
 
-#getting usage data for current monthly period and converting it to a string
+# getting usage data for current monthly period and converting it to a string
 used = commands.getstatusoutput("sudo vnstat --oneline | cut -d ';' -f 11")
 used =str(used)
 
-#getting usage units
+# getting usage units
 usageunits = used[10:13]
 
-#set units to GiB
+# set units to GiB
 units = "GiB"
 
-#getting actual usage value
+# getting actual usage value
 usage = used[5:9]
 
-#checking usage
+# checking usage
 if usageunits == units:
     if usage > limit:
         smtpObj = smtplib.SMTP('smtp.server.com')
--- vnstat_monitor.py
+++ vnstat_monitor.py
@@ -3,30 +3,30 @@
 import os
 import commands
 
-#limit in GB and conversion to GiB
+# limit in GB and conversion to GiB
 limitGB = "100"
 
 limitGB = int(limitGB)
 limit = limitGB * 0.93
 limit = str(limit)
 
-#updating vnStat database
+# updating vnStat database
 os.system("sudo vnstat -u")
 
-#getting usage data for current monthly period and converting it to a string
+# getting usage data for current monthly period and converting it to a string
 used = commands.getstatusoutput("sudo vnstat --oneline | cut -d ';' -f 11")
-used =str(used)
+used = str(used)
 
-#getting usage units
+# getting usage units
 usageunits = used[10:13]
 
-#set units to GiB
+# set units to GiB
 units = "GiB"
 
-#getting actual usage value
+# getting actual usage value
 usage = used[5:9]
 
-#checking usage
+# checking usage
 if usageunits == units:
     if usage > limit:
         os.system("sudo shutdown")
